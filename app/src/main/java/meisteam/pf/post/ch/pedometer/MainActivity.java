a231186e981412215138b6b82c9d5d07ed494cd0
bbortt <timon.borter@gmx.ch>
2016-11-04 14:57:57 +0100

    inital commit

---
+++ app/src/main/java/meisteam/pf/post/ch/pedometer/MainActivity.java
@@ -0,0 +1,14 @@
+
+package meisteam.pf.post.ch.pedometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
